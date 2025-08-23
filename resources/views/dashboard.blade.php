5d6734d5f12656c2cefbce60fe819a204eba5a0e
taimoor5356 <mtaimoornaveed@gmail.com>
2025-08-23 21:16:17 +0500

    updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -70,7 +70,34 @@
                         <div class="card-body">
                             <span class="d-block mb-1"><i class="menu-icon tf-icons bx bx-money text-danger"></i> Total Expenses</span>
                             <h3 class="card-title text-nowrap mb-2">Rs {{!empty($expensesSummary) ? number_format($expensesSummary->total_expenses, 2) : 0}}</h3>
-                            <small>(Advance Salaries: {{number_format(\App\Models\Expense::where('expense_type_id', 2)->whereDate('created_at', request('date_range'))->sum('amount'), 2)}})</small><br>
+@php
+    $dateRange = request('date_range');
+    $query = \App\Models\Expense::query();
+
+    if ($dateRange) {
+        [$start, $end] = explode(' - ', $dateRange);
+        $start = \Carbon\Carbon::parse($start)->startOfDay();
+        $end   = \Carbon\Carbon::parse($end)->endOfDay();
+    }
+@endphp
+
+<small>
+    (Advance Salaries: 
+    {{ number_format(
+        \App\Models\Expense::where('expense_type_id', 2)
+            ->when($dateRange, fn($q) => $q->whereBetween('created_at', [$start, $end]))
+            ->sum('amount'), 2)
+    }})
+</small><br>
+
+<small>
+    (Carwash Expenses: 
+    {{ number_format(
+        \App\Models\Expense::where('expense_type_id', '!=', 2)
+            ->when($dateRange, fn($q) => $q->whereBetween('created_at', [$start, $end]))
+            ->sum('amount'), 2)
+    }})
+</small><br>
                             <a href="{{route('admin.expenses.list')}}" class="btn btn-danger rounded btn-xs">View More</a>
                         </div>
                     </div>
